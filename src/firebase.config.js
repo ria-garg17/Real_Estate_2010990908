05d14acff3b02fcfd476c63bb533466a4a11cb4d
ria-garg17 <72888835+ria-garg17@users.noreply.github.com>
2023-02-14 18:07:52 +0530

    Commit changes for Real Estate

---
+++ src/firebase.config.js
@@ -0,0 +1,42 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCTR0NbKvl-BkRSh_7fpjqU2vusBLl1078",
+//   authDomain: "house-market-e61c9.firebaseapp.com",
+//   projectId: "house-market-e61c9",
+//   storageBucket: "house-market-e61c9.appspot.com",
+//   messagingSenderId: "142403101495",
+//   appId: "1:142403101495:web:286244ccbe5b75900e30b2",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const db = getFirestore();
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPPgRuBco4KA8mWmiWPAK5aLwha6_AyUs",
+  authDomain: "real-estate-3491f.firebaseapp.com",
+  projectId: "real-estate-3491f",
+  storageBucket: "real-estate-3491f.appspot.com",
+  messagingSenderId: "525222116682",
+  appId: "1:525222116682:web:fa1a5e6aac225845c2d5b3",
+  measurementId: "G-8YT2DEBMZP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
